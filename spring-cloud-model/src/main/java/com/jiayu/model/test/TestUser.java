b41e7aa2ba903c2d7a1caba30fd453c067d9e1ad
wb525678 <wb-jcy525678@alibaba-inc.com>
2020-04-10 19:06:32 +0800

    初步搭建

---
+++ spring-cloud-model/src/main/java/com/jiayu/model/test/TestUser.java
@@ -0,0 +1,18 @@
+package com.jiayu.model.test;
+
+import lombok.Data;
+
+/**
+ * @author morningrain
+ * @Description 测试类
+ * @Data 2020/4/10
+ */
+@Data
+public class TestUser {
+
+    private Long id;
+
+    private String userName;
+
+    private String address;
+}
