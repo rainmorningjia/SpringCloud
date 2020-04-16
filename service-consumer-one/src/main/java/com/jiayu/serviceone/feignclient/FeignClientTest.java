68a073f3cf596c4e0b18b3526aaf7d2fe92d000e
wb525678 <wb-jcy525678@alibaba-inc.com>
2020-04-16 11:03:32 +0800

    尝试restTemplate

--- service-consumer-one/src/main/java/com/jiayu/serviceone/feignclient/FeignClientTest.java
+++ service-consumer-one/src/main/java/com/jiayu/serviceone/feignclient/FeignClientTest.java
@@ -1,6 +1,8 @@
 package com.jiayu.serviceone.feignclient;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient(name = "service-provider", fallback = FeignClientFallback.class)
 public interface FeignClientTest {
 
+    @LoadBalanced
     @RequestMapping(value = "/serviceProvider/test", method = RequestMethod.GET)
     String getServiceTwoInfo();
 
